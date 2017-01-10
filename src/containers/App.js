ffa1b02b47221afce5016aa7f7ac01fe484d03ec
sanasov <igrey@mail.ru>
2017-01-10 12:53:24 +0300

    + combine reducers + components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import User from '../components/User'
+import Page from '../components/Page'
+
 class App extends Component {
     render() {
-        const {name, surname, age} = this.props.user
+        const {user, page} = this.props
         return <div>
-            <p>Привет из App, {name} {surname}!</p>
-            <p>Тебе уже {age} ?</p>
+            <User name={user.name}/>
+            <Page photos={page.photos} year={page.year}/>
         </div>
+
     }
 }
 function mapStateToProps(state) {
     return {
-        user: state
+        user: state.user,
+        page: state.page
     }
 }
 export default connect(mapStateToProps)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-const initialState = {
-    name: 'Василий',
-    surname: 'Реактов',
-    age: 27
-};
-export default function userstate(state = initialState) {
-    return state;
-}
+import {combineReducers} from 'redux'
+import page from './page'
+import user from './user'
+
+export default combineReducers({
+    page,
+    user
+})
